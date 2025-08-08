5d7321c52fb447d235def36bd6e2f44e59eedd26
Juan Sebastian Jaramillo Guerra -X (juajaram - CONCENTRIX CVG CUSTOMER MANAGEMENT GROUP INC at Cisco) <juajaram@cisco.com>
2025-08-07 22:05:37 -0500

    App e Histogramas

--- app.py
+++ app.py
@@ -2,11 +2,27 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 
-st.header("Análisis de anuncios de venta de coches")
+st.header("Analisis de anuncios de venta de coches")
 
-car_data = pd.read_csv('vehicles_us.csv')  # asegúrate de subir luego este archivo
+# Cargar datos
+car_data = pd.read_csv('vehicles_us.csv')
 
-if st.button('Mostrar histograma'):
-    st.write('Histograma de odómetro')
+# Boton  histogramas
+if st.button("Mostrar histogramas"):
+    # H.Odometer
+    st.subheader("Histograma: odometer")
     fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
+    fig.update_layout(title='Distribución del odometro', xaxis_title='Odometro', yaxis_title='Frecuencia')
+    st.plotly_chart(fig, use_container_width=True)
+
+    # H.Price
+    st.subheader("Histograma: price")
+    fig = go.Figure(data=[go.Histogram(x=car_data['price'])])
+    fig.update_layout(title='Distribucion del precio', xaxis_title='Precio', yaxis_title='Frecuencia')
+    st.plotly_chart(fig, use_container_width=True)
+
+    # H.Model_year
+    st.subheader("Histograma: model_year")
+    fig = go.Figure(data=[go.Histogram(x=car_data['model_year'].dropna())])
+    fig.update_layout(title='Distribucion del año del modelo', xaxis_title='Año del modelo', yaxis_title='Frecuencia')
     st.plotly_chart(fig, use_container_width=True)
